f410419820bb2e55da4ad87bf51ff4fa2638d04e
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2022-06-03 14:04:14 +0200

    Refactor

--- src/builder.py
+++ src/builder.py
@@ -6,32 +6,29 @@ import time
 import shutil
 import subprocess
 import datetime
+import sugarversion
+from ftplib import FTP_TLS
 
-# Command: python3 src/builder.py -b cht-dev.sugaropencloud.tar.gz
+# Command: python3 src/builder.py -b cht-dev.sugaropencloud.tar.gz -p 2Cp5WvCxJJykcyy
 
 # Sets up a Sugar instance based on a backup
 class Builder():
 
-    def __init__(self):
+    def __init__(self, version, backup, ftpUser, ftpPwd):
         cwd = os.getcwd()
         self.dataPath = cwd+"/data/"
         self.dbPath = cwd+"/mysql/"
-
-    def setBackupName(self, backupName):
-        self.backupName = backupName
-
-    def setSQLFileName(self, sqlFileName):
-        self.sqlFileName = sqlFileName
-
-    def setInstanceFolderName(self, instanceFolderName):
-        self.instanceFolderName = instanceFolderName
+        self.sugarVersion = sugarversion.SugarVersion(version)
+        self.backupName = backup
+        self.ftpPwd = ftpPwd
+        self.ftpUser = ftpUser
 
     def setupInstance(self):
         print("1. Cleanup...")
         self.cleanup()
 
-        print("2. Unzip backup...")
-        self.extractBackup()
+        print("2. Download sources...")
+        self.download()
 
         print("3. Configure instance...")
         self.configureInstance()
@@ -52,14 +49,31 @@ class Builder():
             shutil.rmtree(self.dbPath)
         os.mkdir(self.dataPath)
 
+    def download(self):
+        print("Download backup...")
+        with FTP_TLS('ftp.sugarcrm.eu', self.ftpUser, self.ftpPwd) as ftp:
+            ftp.prot_p()
+            with open(os.getcwd()+"/"+self.backupName, 'wb') as local_file:
+                ftp.retrbinary("RETR "+self.backupName, local_file.write)
+            ftp.quit()
+            self.extractBackup()
+        print("Download upgrader...")
+        os.system("wget -P "+self.dataPath+" "+self.sugarVersion.getSilentUpgrader())
+        os.system("unzip -qq "+self.dataPath+"/"+self.sugarVersion.getUpgraderFile()+" -d "+self.dataPath+"/upgrader")
+        os.system("wget -P "+self.dataPath+" "+self.sugarVersion.getUpgradePackage())
+        # Rename upgrade package to standard name so it can be used in Jenkins File
+        os.rename(self.dataPath+"/"+self.sugarVersion.getPackageFile(), self.dataPath+"/upgrade.zip")
+        print("Download tests...")
+        os.system("wget -P "+self.dataPath+" "+self.sugarVersion.getPHPUnitTests())
+        os.system("unzip -qq -o "+self.dataPath+"/"+self.sugarVersion.getUnitTestsFile()+" -d data/sugar")
+
     def extractBackup(self):
-        # workaround, as Jenkins S3 plugin downloads the file into a folder with the same name
-        tar = tarfile.open(os.getcwd()+"/"+self.backupName+"/"+self.backupName, "r:gz")
+        tar = tarfile.open(os.getcwd()+"/"+self.backupName, "r:gz")
         members = []
         for member in tar.getmembers():
-            if self.instanceFolderName in member.path and not member.name.endswith("sql") :
+            if self.sugarVersion.getInstanceFolderName() in member.path and not member.name.endswith("sql") :
                 members.append(member)
-            if member.isfile() and member.name.endswith(self.sqlFileName):
+            if member.isfile() and member.name.endswith(self.sugarVersion.getSQLFileName()):
                 members.append(member)
         tar.extractall(self.dataPath, members)
         tar.close()
@@ -67,15 +81,15 @@ class Builder():
         time.sleep(3)
         for item in os.listdir(self.dataPath):
             if os.path.isdir(os.path.join(self.dataPath, item)):
-                shutil.move(self.dataPath+"/"+item+"/"+self.instanceFolderName, self.dataPath+"/sugar")
-                shutil.move(self.dataPath+"/"+item+"/"+self.sqlFileName, self.dataPath+"/"+self.sqlFileName)
+                shutil.move(self.dataPath+"/"+item+"/"+self.sugarVersion.getInstanceFolderName(), self.dataPath+"/sugar")
+                shutil.move(self.dataPath+"/"+item+"/"+self.sugarVersion.getSQLFileName(), self.dataPath+"/"+self.sugarVersion.getSQLFileName())
 
     def importDatabase(self):
         # Create database
         proc = subprocess.Popen(["docker exec sugar-mysql mysql -u root -proot -e 'create database sugar'"], stdout=subprocess.PIPE, shell=True, universal_newlines=True)
         proc.wait()
         # Import file to database
-        proc = subprocess.Popen(["cat "+self.dataPath+self.sqlFileName+" | docker exec -i sugar-mysql mysql -u root -proot sugar"], stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+        proc = subprocess.Popen(["cat "+self.dataPath+self.sugarVersion.getSQLFileName()+" | docker exec -i sugar-mysql mysql -u root -proot sugar"], stdout=subprocess.PIPE, shell=True, universal_newlines=True)
         proc.wait()
         # Set admin password
         proc = subprocess.Popen(["cat src/sugarconfig/admin_pwd.sql | docker exec -i sugar-mysql mysql -u root -proot sugar"], stdout=subprocess.PIPE, shell=True, universal_newlines=True)
@@ -96,12 +110,10 @@ class Builder():
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--backup",  required=True , help = "Name of the backup file")
-parser.add_argument("-i", "--instance", default="sugar1130ent" , help = "Name of the instance folder in the backup")
-parser.add_argument("-s", "--sql", default="sugar1130ent.sql" , help = "Name of the sql file in the backup")
+parser.add_argument("-v", "--version", default="1130" , help = "The Sugar Version")
+parser.add_argument("-p", "--ftppdwd", required=True , help = "Password to access the FTP server to download the backup")
+parser.add_argument("-u", "--ftpuser", required=True , help = "User to access the FTP server to download the backup")
 args = parser.parse_args()
 
-builder = Builder()
-builder.setBackupName(args.backup)
-builder.setInstanceFolderName(args.instance)
-builder.setSQLFileName(args.sql)
+builder = Builder(args.version, args.backup, args.ftpuser, args.ftppwd)
 builder.setupInstance()
---
+++ src/sugarversion.py
@@ -0,0 +1,45 @@
+
+
+class SugarVersion():
+
+    def __init__(self, version):
+        self.version = version
+
+    def getSilentUpgrader(self):
+        if (self.version == '1130'):
+            return "https://honeycomb.sugarcrm.io/download/release/12.0.0/265/silentUpgrade-PRO-12.0.0.zip"
+        if (self.version == '1200'):
+            return "https://honeycomb.sugarcrm.io/download/train/12.1.0/134/silentUpgrade-PRO-12.1.0.zip"
+        else:
+            print(self.version+" is not a supported Sugar Version!")
+
+    def getUpgradePackage(self):
+        if (self.version == '1130'):
+            return "https://honeycomb.sugarcrm.io/download/release/12.0.0/265/SugarEnt-Upgrade-11.3.0-to-12.0.0.zip"
+        if (self.version == '1200'):
+            return "https://honeycomb.sugarcrm.io/download/train/12.1.0/134/SugarEnt-Upgrade-12.0.0-to-12.1.0.zip"
+        else:
+            print(self.version+" is not a supported Sugar Version!")
+
+    def getUpgraderFile(self):
+        return self.getSilentUpgrader().split("/")[-1]
+
+    def getPackageFile(self):
+        return self.getUpgradePackage().split("/")[-1]
+
+    def getPHPUnitTests(self):
+        if (self.version == '1130'):
+            return "https://honeycomb.sugarcrm.io/download/release/11.3.0/236/SugarEnt-11.3.0-tests.zip"
+        if (self.version == '1200'):
+            return "https://honeycomb.sugarcrm.io/download/train/12.1.0/134/SugarEnt-12.1.0-tests.zip"
+        else:
+            print(self.version+" is not a supported Sugar Version!")
+
+    def getUnitTestsFile(self):
+        return self.getPHPUnitTests().split("/")[-1]
+
+    def getInstanceFolderName(self):
+        return "sugar"+self.version+"ent"
+
+    def getSQLFileName(self):
+        return self.getInstanceFolderName()+".sql"
