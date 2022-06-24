e9e432f092fd3bdac8d08a1ea293b183d6b09fe7
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2022-06-24 20:13:12 +0200

    syntax correction

---
+++ src/installer.py
@@ -0,0 +1,61 @@
+import requests
+import json
+import os.path
+import argparse
+import time
+
+# Command: python3 src/installer.py -a "ZHINST Phase 1 S27"
+class Installer():
+
+    def setSugarURL(self, sugarURL):
+        self.sugarAuthURL = sugarURL+"/rest/v11/oauth2/token"
+        self.sugarPackagesURL = sugarURL+"/rest/v11/Administration/packages"
+
+    def setPackageName(self, packageName):
+        generatedPackage = time.strftime("%Y%m%d")+"_"+packageName.replace(" ", "_")+".zip"
+        if (os.path.exists(os.getcwd()+"/"+generatedPackage)):
+            self.package = os.getcwd()+"/"+generatedPackage
+        else:
+            self.package = os.getcwd()+"/"+packageName
+
+    def setUserName(self, username):
+        self.username = username
+
+    def setPwd(self, pwd):
+        self.pwd = pwd
+
+    def run(self):
+        print("Installing package "+self.package)
+        # 1. Authenticate
+        data = { "grant_type":"password",
+                "client_id":"sugar",
+                "client_secret":"",
+                "username":self.username,
+                "password":self.pwd,
+                "platform":"base"}
+        authResponse = requests.post(url = self.sugarAuthURL, data = data)
+        # 2. Upload package
+        token = authResponse.json()["access_token"]
+        header = {"oauth-token" : token}
+        files = {"upgrade_zip": open(self.package,'rb')}
+        uploadResponse = requests.post(url = self.sugarPackagesURL, headers=header, files=files)
+        # 3. Install package
+        packageID = uploadResponse.json()["file_install"]
+        installResponse = requests.get(url = self.sugarPackagesURL+"/"+packageID+"/install", headers=header)
+        if (installResponse.json()["status"]!="installed"):
+            raise Exception("Package installation failed: "+installResponse.json())
+        print("Installation status: "+installResponse.json()["status"])
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--url",  default="http://localhost/sugar/" , help = "URL of the Sugar instance")
+parser.add_argument("-a", "--name",  required=True , help = "Name of the package")
+parser.add_argument("-u", "--user", default="admin" , help = "Admin user of the Sugar instance")
+parser.add_argument("-p", "--pwd", default="Password123" , help = "Password of the admin user")
+args = parser.parse_args()
+
+installer = Installer()
+installer.setSugarURL(args.url)
+installer.setPackageName(args.name)
+installer.setUserName(args.user)
+installer.setPwd(args.pwd)
+installer.run()
