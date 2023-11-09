b03c19e8d48b50f549a7043b11fb99f242ac56bc
nbleyh-sugarcrm <nbleyh@sugarcrm.com>
2023-11-09 16:36:48 +0100

    Ensure valid admin user

--- src/sugarconfig/admin_pwd.sql
+++ src/sugarconfig/admin_pwd.sql
@@ -4,3 +4,7 @@ UPDATE sugar.users SET is_admin = '1'
 WHERE id = '1';
 UPDATE sugar.users SET user_hash = '$2y$10$ua6PicOvqyYMKgOR6gzFcub.Z5s40j6moWRH4oaO.Ef667lz.nb0m'
 WHERE id = '1';
+UPDATE sugar.users SET status = 'Active'
+WHERE id = '1';
+UPDATE sugar.users SET deleted = '0'
+WHERE id = '1';
